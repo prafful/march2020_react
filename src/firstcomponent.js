4e1e7a080c9e6e9a7166b65091f23c0cd0c36385
Prafful Daga <prraful@gmail.com>
2020-03-10 16:37:32 +0530

    first component in react

---
+++ src/firstcomponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class MyFirstComponent extends React.Component{
+
+    render(){
+        return(<h1>
+                I am JSX. I am not HTML!
+            </h1>)
+    }
+
+}
+
+export default MyFirstComponent
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import MyFirstComponent from './firstcomponent';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+var location = document.getElementById('rootcomponent')
+
+ReactDOM.render(<MyFirstComponent></MyFirstComponent>  , location)
