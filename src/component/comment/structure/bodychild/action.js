7fc602f721e100231611433c6e7c994270957aac
Prafful Daga <prraful@gmail.com>
2020-03-11 16:31:15 +0530

    states in react

--- src/component/comment/structure/bodychild/action.js
+++ src/component/comment/structure/bodychild/action.js
@@ -3,17 +3,40 @@ import React from 'react';
 
 
 class CommentAction extends React.Component {
+
+    
+
+    constructor(props){
+        super(props)
+        //define and initalize state
+        this.state = {
+            slikes: parseInt(this.props.calikes),
+            sdislikes: parseInt(this.props.cadislikes),
+        }
+
+        this.increment = this.increment.bind(this)
+
+    }
+
+    increment = function(){
+        console.log("In increment function!" );
+        //this.state.score = this.state.score + 1
+        //update the state -> score
+        //console.log(typeof(parseInt(this.state.slikes)));
+        this.setState({slikes: this.state.slikes + 1})
+     }
     
     render() { 
         return (
             <div>
-                Likes: {this.props.calikes}
+                Likes: {this.state.slikes}
                 &nbsp;
-                Dislikes: {this.props.cadislikes}
+                Dislikes: {this.state.sdislikes}
                 &nbsp;
-                <button> + </button>
+                <button onClick={this.increment}> + </button>
                 &nbsp;
                 <button > - </button>
+               
             </div>
           );
     }
